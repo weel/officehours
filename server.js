690cf0c67764f8db51759820d4721d3bc53ec610
Morten Weel Johnsen <mortjohn@iterate.no>
2014-02-05 10:30:36 +0100

    Adding port from process

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ var app = express();
 app.use(express.static(__dirname));
 
 https.createServer({key: privateKey, cert: certificate}, app).listen(8000);
-http.createServer(app).listen(8001);
+http.createServer(app).listen(process.env.PORT);
 
 console.log('running on https://localhost:8000 and http://localhost:8001');
